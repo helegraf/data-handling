9e6633b166ac52f7313079f47d565cc32ca38c85
Helena Graf <Graf.Helena@web.de>
2019-07-10 01:24:31 +0200

    data handling update

--- data-handler/src/test/java/dataHandlingTest.java
+++ data-handler/src/test/java/dataHandlingTest.java
@@ -7,30 +7,31 @@ public class dataHandlingTest {
 		MetaDataDataBaseConnection databaseConn = new MetaDataDataBaseConnection(args[0], args[1], args[2], args[3]);
 		
 		System.out.println("Test available sets");
-		System.out.println(databaseConn.getAvailableClassifierSets());
-		System.out.println(databaseConn.getAvailableDataSetSets());
-		System.out.println(databaseConn.getAvailableMetaDataSets());
+//		System.out.println(databaseConn.getAvailableClassifierSets());
+//		System.out.println(databaseConn.getAvailableDataSetSets());
+//		System.out.println(databaseConn.getAvailableMetaDataSets());
 		
 		String datasetOrigin = "openML_dataset_id";
 		String classifierSetName = "all-standard_config";
-		String dataSetSetName="all";
+		String dataSetSetName="metaminer_small";
 		String setName = classifierSetName;
 		String metaDataSetName="all";
 		int datasetId = 3;
 		
-		System.out.println("Test available classifier data");
-		System.out.println(databaseConn.getClassifierPerformancesForDataSet(3, datasetOrigin, classifierSetName));
-		System.out.println(databaseConn.getClassifierPerformancesForDataSetSet(dataSetSetName, classifierSetName));
-		
-		System.out.println("Test available members of data sets");
-		System.out.println(databaseConn.getMembersOfClassifierSet(setName));
+//		System.out.println("Test available classifier data");
+//		System.out.println(databaseConn.getClassifierPerformancesForDataSet(3, datasetOrigin, classifierSetName));
+//		System.out.println(databaseConn.getClassifierPerformancesForDataSetSet(dataSetSetName, classifierSetName));
+//		
+//		System.out.println("Test available members of data sets");
+//		System.out.println(databaseConn.getMembersOfClassifierSet(setName));
 		setName=dataSetSetName;
 		System.out.println(databaseConn.getMembersOfDatasetSet(setName));
-		setName=metaDataSetName;
-		System.out.println(databaseConn.getMembersOfMetadataSet(setName));
-		
-		System.out.println("Test available metafeature data");
-		System.out.println(databaseConn.getMetaDataSetForDataSet(datasetId, datasetOrigin, metaDataSetName));
-		System.out.println(databaseConn.getMetaDataSetForDataSetSet(dataSetSetName, metaDataSetName));
+		System.out.println(databaseConn.getMembersOfDatasetSet(setName).get(0).getClass().getName());
+//		setName=metaDataSetName;
+//		System.out.println(databaseConn.getMembersOfMetadataSet(setName));
+//		
+//		System.out.println("Test available metafeature data");
+//		System.out.println(databaseConn.getMetaDataSetForDataSet(datasetId, datasetOrigin, metaDataSetName));
+//		System.out.println(databaseConn.getMetaDataSetForDataSetSet(dataSetSetName, metaDataSetName));
 	}
 }
